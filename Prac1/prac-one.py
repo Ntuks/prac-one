11db45d005c09595f8d6a82e1b7c6e0f5af29f1a
mr-mpaku <ntuthuko2@live.com>
2019-08-04 23:56:30 +0200

    changed the wrong implementation

--- Prac1/prac-one.py
+++ Prac1/prac-one.py
@@ -10,6 +10,7 @@ Description: Testing how to switch an LED on with a button that is configured by
 # import Relevant Librares
 import RPi.GPIO as GPIO
 from time import sleep
+import itertools as iter
 
 # Sleep time for the buttons to wait long enough
 SLEEPTIME = 2
@@ -29,101 +30,48 @@ def init():
         GPIO.output(LED, False)
 
     # Setting up the input pin the BUTTON & Using the Broadcom chip's pull-up resistor -
-    # so that we don't physically configure it in our breadboard - these are for debouncing
+    # so that we don't physically configure iter in our breadboard - these are for debouncing
     GPIO.setup(BTNS[0], GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(BTNS[1], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    GPIO.add_event_detect(BTNS[0], GPIO.RISING)  # add rising edge detection on a BTNS
-    GPIO.add_event_detect(BTNS[1], GPIO.RISING)  # add rising edge detection on a BTNS
+    GPIO.add_event_detect(BTNS[0], GPIO.RISING, callback=binaryIncrement, bouncetime=200)  # add rising edge detection on a BTNS
+    GPIO.add_event_detect(BTNS[1], GPIO.RISING, callback=binaryDecrement, bouncetime=200)  # add rising edge detection on a BTNS
 
 
 # Logic that you write
 def main():
     print("Waiting For Button to be pressed")
-    if GPIO.event_detected(BTNS[0]):
-        print("Binary Increment")
-        binaryIncrement()
-    elif GPIO.event_detected(BTNS[1]):
-        print("Binary Decrement")
-        binaryDecrement()
 
 """ THIS FUNCTION LIGHTS UP THE LEDS IN A BINARY COUNTING INCREMENTING FASHION """
-def binaryIncrement():
-    # Starting off with all the LEDs ON == 001
-    GPIO.output(LEDS[2], GPIO.input(BTNS[0]))
-    sleep(SLEEPTIME)
+def binaryIncrement(channel):
+    print("Binary Increment")
+    # This code is for making sure that the counter wraps around.
+    global index 
+    if index == len(values)-1:
+        index = 0
+    else:
+        index+1    
 
-    # Switching off LED2 & Switching on LED1 == 010
-    GPIO.output(LEDS[2], False)
-    GPIO.output(LEDS[1], GPIO.input(BTNS[0]))
-    sleep(SLEEPTIME)
-
-    # Switching on LED2 again == 011
-    GPIO.output(LEDS[2], GPIO.input(BTNS[0]))
-    sleep(SLEEPTIME)
-
-    # Switching off LED1 & LED2 again & Switching on LED0 == 100
-    GPIO.output(LEDS[2], False)
-    GPIO.output(LEDS[1], False)
-    GPIO.output(LEDS[0], GPIO.input(BTNS[0]))
-    sleep(SLEEPTIME)
-
-    # Switching on LED2 == 101
-    GPIO.output(LEDS[2], GPIO.input(BTNS[0]))
-    sleep(SLEEPTIME)
-
-    # Switching on LED2 == 110
-    GPIO.output(LEDS[2], False)
-    GPIO.output(LEDS[1], GPIO.input(BTNS[0]))
-    sleep(SLEEPTIME)
-
-    # Starting on LED1 == 111
-    GPIO.output(LEDS[2], GPIO.input(BTNS[0]))
-    sleep(SLEEPTIME)
-
-    for LED in LEDS:
-        GPIO.output(LED, False)
-    sleep(SLEEPTIME)
+    ON = list(iter.compress(LEDs, values[index]))   
+    OFF = [LED for LED in LEDs if LED not in ON]    
+    GPIO.output(ON, GPIO.HIGH)
+    GPIO.output(OFF, GPIO.LOW)
 
 """ THIS FUNCTION LIGHTS UP THE LEDS IN A BINARY COUNTING DECREMENTING FASHION """
-def binaryDecrement():
-    # Starting off with all the LEDs ON == 111
-    for LED in LEDS:
-        GPIO.output(LED, GPIO.input(BTNS[1]))
-    sleep(SLEEPTIME)
+def binaryDecrement(channel):
+    # This code is for making sure that the counter wraps around.
+    print("Binary Decrement")
+    global index
+    if index == 0:
+        index = len(values)-1  
+    else:
+        index-1    
 
-    # Switching off LED2 == 110
-    GPIO.output(LEDS[2], False)
-    sleep(SLEEPTIME)
-
-    # Switching off LED1 & Switching on LED2 back == 101
-    GPIO.output(LEDS[1], False)
-    GPIO.output(LEDS[2], GPIO.input(BTNS[1]))
-    sleep(SLEEPTIME)
-
-    # Switching off LED2 again == 100
-    GPIO.output(LEDS[2], False)
-    sleep(SLEEPTIME)
-
-    # Switching on LED1 & LED2 again & Switching off LED0 == 011
-    GPIO.output(LEDS[1], GPIO.input(BTNS[1]))
-    GPIO.output(LEDS[2], GPIO.input(BTNS[1]))
-    GPIO.output(LEDS[0], False)
-    sleep(SLEEPTIME)
-
-    # Switching off LED1 & Switching on LED2 back == 010
-    GPIO.output(LEDS[2], False)
-    sleep(SLEEPTIME)
-
-    # Switching off LED2 agaain == 001
-    GPIO.output(LEDS[1], False)
-    GPIO.output(LEDS[2], GPIO.input(BTNS[1]))
-    sleep(SLEEPTIME)
-
-    # Starting off with all the LEDs ON == 111
-    for LED in LEDS:
-        GPIO.output(LED, False)
-    sleep(SLEEPTIME)
+    ON = list(iter.compress(LEDs, values[index]))
+    OFF = [LED for LED in LEDs if LED not in ON]
+    GPIO.output(ON, GPIO.HIGH)
+    GPIO.output(OFF, GPIO.LOW)
+    
 
 
 # Only run the functions if
